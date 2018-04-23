033551c6a59a0e888263057c02d6f02f4531e092
arminbar <armin.bartnik@web.de>
2018-04-23 17:38:40 +0200

    updated cname and log.js to new domain and improved the index.html

--- log.js
+++ log.js
@@ -1,4 +1,4 @@
 var dns = require('dns');
-dns.resolve('bartsonb.de', 'TXT', function(err, addresses) {
+dns.resolve('klangboahrd.de', 'TXT', function(err, addresses) {
     console.log(err, addresses);
 });
